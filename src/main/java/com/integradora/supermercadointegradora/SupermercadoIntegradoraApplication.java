6e293aeba7ee4319b450f3a7009f41ccf37267c2
Alison <20233tn221@utez.edu.mx>
2024-12-05 14:29:26 -0600

    primer commit

---
+++ src/main/java/com/integradora/supermercadointegradora/SupermercadoIntegradoraApplication.java
@@ -0,0 +1,13 @@
+package com.integradora.supermercadointegradora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermercadoIntegradoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermercadoIntegradoraApplication.class, args);
+    }
+
+}
