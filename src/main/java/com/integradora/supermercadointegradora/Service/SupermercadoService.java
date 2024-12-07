d4caab7d4344e606fdda722e369470045d30ea0d
Alison <20233tn221@utez.edu.mx>
2024-12-07 02:13:31 -0600

    alis11 agrege codigo en SupermercadoService

--- src/main/java/com/integradora/supermercadointegradora/Service/SupermercadoService.java
+++ src/main/java/com/integradora/supermercadointegradora/Service/SupermercadoService.java
@@ -1,6 +1,7 @@
 package com.integradora.supermercadointegradora.Service;
 
 import com.integradora.supermercadointegradora.Entity.CarritoProducto;
+import com.integradora.supermercadointegradora.Entity.Producto;
 import com.integradora.supermercadointegradora.repository.CarritoProductoRepository;
 import com.integradora.supermercadointegradora.repository.ClienteRepository;
 import com.integradora.supermercadointegradora.repository.ProductoRepository;
@@ -26,4 +27,25 @@ public class SupermercadoService {
     private Stack<CarritoProducto> historialEliminados = new Stack<>();
 
     // Aqui agregas lo que sigue ali
-}
+
+    // se vaa procesar la compra de un cliente
+    public String procesarCompra(Long clienteId) {
+        // se obtendra el carrito de productos del cliente
+        List<CarritoProducto> productosEnCarrito = carritoProductoRepository.findByClienteId(clienteId);
+
+        if (productosEnCarrito.isEmpty()) {
+            return "El carrito del cliente esta vacio";
+        }
+        // aqui se calculara el total de la compra
+        double total = 0;
+        for (CarritoProducto carritoProducto : productosEnCarrito) {
+            Producto producto = carritoProducto.getProducto();
+            total += producto.getPrecio() * carritoProducto.getCantidad();
+        }
+
+        // vaciar el carrito del cliente despues de la compra
+        carritoProductoRepository.deleteAll(productosEnCarrito);
+
+        return "Compra finalizada. Total: "+total;
+     }
+    }
