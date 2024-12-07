f2a7163887389557e54b3047209a96a65b05e68b
Alison <20233tn221@utez.edu.mx>
2024-12-07 01:43:49 -0600

    alis8 agrege parte de codigo CarritoProductoService

--- src/main/java/com/integradora/supermercadointegradora/Service/CarritoProductoService.java
+++ src/main/java/com/integradora/supermercadointegradora/Service/CarritoProductoService.java
@@ -2,6 +2,8 @@ package com.integradora.supermercadointegradora.Service;
 
 
 import com.integradora.supermercadointegradora.Entity.CarritoProducto;
+import com.integradora.supermercadointegradora.Entity.Cliente;
+import com.integradora.supermercadointegradora.Entity.Producto;
 import com.integradora.supermercadointegradora.repository.CarritoProductoRepository;
 import com.integradora.supermercadointegradora.repository.ClienteRepository;
 import com.integradora.supermercadointegradora.repository.ProductoRepository;
@@ -28,4 +30,28 @@ public class CarritoProductoService {
 
 
     //Falta agregar cosas
+    //metodo que agregara un producto al carrito
+    public CarritoProducto agregarProductoAlCarrito(Long clienteId, Long productoId, int cantidad) {
+        // se va a buscar al cliente con su id
+        Optional<Cliente> clienteOpt = clienteRepository.findById(clienteId);
+        //se va a buscar el producto cin su id
+        Optional<Producto> productoOpt = productoRepository.findById(productoId);
+        //se checara que el cliente y producto existan
+        if (clienteOpt.isPresent() && productoOpt.isPresent()) {
+            // Se obtiene el cliente y el producto de los Optional con get
+            Cliente cliente = clienteOpt.get();
+            Producto producto = productoOpt.get();
+
+            //instancia para la relacion cliente-produ-cant
+            CarritoProducto carritoProducto = new CarritoProducto();
+            carritoProducto.setCliente(cliente);
+            carritoProducto.setProducto(producto);
+            carritoProducto.setCantidad(cantidad);
+
+            //se va a guardar el ob CarritoProducto
+            return carritoProductoRepository.save(carritoProducto);
+        }
+
+        return null;
+}
 }
