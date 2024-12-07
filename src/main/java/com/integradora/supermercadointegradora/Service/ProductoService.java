85b87d15355d92633994056f4d26fd975cee599a
Alison <20233tn221@utez.edu.mx>
2024-12-07 02:03:50 -0600

    alis10 agrege el codigo de ProductoService

--- src/main/java/com/integradora/supermercadointegradora/Service/ProductoService.java
+++ src/main/java/com/integradora/supermercadointegradora/Service/ProductoService.java
@@ -1,4 +1,18 @@
 package com.integradora.supermercadointegradora.Service;
 
+import com.integradora.supermercadointegradora.Entity.Producto;
+import com.integradora.supermercadointegradora.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    // Agregar un nuevo producto
+    public Producto agregarProducto(Producto producto) {
+        return productoRepository.save(producto);
+ }
 }
