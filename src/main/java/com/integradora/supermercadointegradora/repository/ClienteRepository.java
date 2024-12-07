e4d64443a7c5a2dfec751d97f6e99960e4f78368
Alison <20233tn221@utez.edu.mx>
2024-12-07 00:54:38 -0600

    alis4 carpeta repository y clienterepository

---
+++ src/main/java/com/integradora/supermercadointegradora/repository/ClienteRepository.java
@@ -0,0 +1,6 @@
+package com.integradora.supermercadointegradora.repository;
+import com.integradora.supermercadointegradora.Entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ClienteRepository extends JpaRepository<Cliente,Long>{
+    }
+
