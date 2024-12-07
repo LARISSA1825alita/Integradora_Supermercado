aeaa798566a2b93c596da20cb6583e9139c87e4a
EFRASSJ <20233tn155@utez.edu.mx>
2024-12-07 03:32:27 -0600

    Agg cosas SupermercadoController y /Agregat Producto  Efra19

---
+++ src/main/java/com/integradora/supermercadointegradora/controller/SupermercadoController.java
@@ -0,0 +1,23 @@
+package com.integradora.supermercadointegradora.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.integradora.supermercadointegradora.Service.SupermercadoService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/supermercado")
+public class SupermercadoController {
+
+    @Autowired
+    private SupermercadoService supermercadoService;
+
+    // Agregar un producto al carrito de un cliente
+    @PostMapping("/agregarProducto")
+    public String agregarProductoAlCarrito(@RequestParam Long clienteId, @RequestParam Long productoId, @RequestParam int cantidad) {
+        return supermercadoService.agregarProductoAlCarrito(clienteId, productoId, cantidad);
+    }
+
+    // Agui agrega uno
+}
