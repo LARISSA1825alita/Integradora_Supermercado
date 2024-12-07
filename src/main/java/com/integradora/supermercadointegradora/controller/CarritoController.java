da78007b7d6d8d65ba5636464811c3314589db41
EFRASSJ <20233tn155@utez.edu.mx>
2024-12-07 03:16:25 -0600

    Agg cosas CarritoController Efra17

--- src/main/java/com/integradora/supermercadointegradora/controller/CarritoController.java
+++ src/main/java/com/integradora/supermercadointegradora/controller/CarritoController.java
@@ -1,4 +1,27 @@
 package com.integradora.supermercadointegradora.controller;
 
+import com.integradora.supermercadointegradora.Service.CarritoProductoService;
+import com.integradora.supermercadointegradora.Entity.CarritoProducto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/carrito")
 public class CarritoController {
+
+    @Autowired
+    private CarritoProductoService carritoProductoService;
+
+    // Endpoint para agregar un producto al carrito
+    @PostMapping("/agregar")
+    public String agregarProductoAlCarrito(@RequestParam Long clienteId, @RequestParam Long productoId, @RequestParam int cantidad) {
+        CarritoProducto carritoProducto = carritoProductoService.agregarProductoAlCarrito(clienteId, productoId, cantidad);
+        if (carritoProducto != null) {
+            return "Producto agregado al carrito.";
+        }
+        return "Hubo un problema al agregar el producto.";
+    }
+
 }
