efc53b1fc72be618cb73bc3d6b9cad8533664a99
EFRASSJ <20233tn155@utez.edu.mx>
2024-12-07 00:47:14 -0600

    Agregue la entidad CarritoProducto Efra3

---
+++ src/main/java/com/integradora/supermercadointegradora/Entity/CarritoProducto.java
@@ -0,0 +1,54 @@
+package com.integradora.supermercadointegradora.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class CarritoProducto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+    private int cantidad;
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
