df34a7f1f3e45679056ba4793f37e031586dd666
EFRASSJ <20233tn155@utez.edu.mx>
2024-12-07 01:34:21 -0600

    Agg cosas al CarritoProductoService Efra8

--- src/main/java/com/integradora/supermercadointegradora/Service/CarritoProductoService.java
+++ src/main/java/com/integradora/supermercadointegradora/Service/CarritoProductoService.java
@@ -1,4 +1,31 @@
 package com.integradora.supermercadointegradora.Service;
 
+
+import com.integradora.supermercadointegradora.Entity.CarritoProducto;
+import com.integradora.supermercadointegradora.repository.CarritoProductoRepository;
+import com.integradora.supermercadointegradora.repository.ClienteRepository;
+import com.integradora.supermercadointegradora.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Stack;
+
+@Service
 public class CarritoProductoService {
+
+    @Autowired
+    private CarritoProductoRepository carritoProductoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    private Stack<CarritoProducto> historialEliminados = new Stack<>();
+
+
+    //Falta agregar cosas
 }
